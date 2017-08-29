7663b24b498942b388e23d4ac54ee8765a7ac0ec
Kittinun Vantasin <kittinun.f@gmail.com>
2017-08-29 13:44:26 +0900

    Add deploy, add codecov

---
+++ deploy_bintray.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ "$TRAVIS_BRANCH" == release-v* ]]; then
+
+  echo "We're on release branch, deploying"
+
+  modules=("fuse")
+
+  for i in "${modules[@]}"
+  do
+    ./gradlew :$i:clean :$i:build :$i:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
+  done
+
+fi
