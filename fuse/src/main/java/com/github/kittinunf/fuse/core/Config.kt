20a0efe848af865f556aefe564f9a9f998689617
Kittinun Vantasin <kittinun.f@gmail.com>
2019-10-31 20:56:42 +0900

    Simplify config

--- fuse/src/main/java/com/github/kittinunf/fuse/core/Config.kt
+++ fuse/src/main/java/com/github/kittinunf/fuse/core/Config.kt
@@ -1,22 +1,10 @@
 package com.github.kittinunf.fuse.core
 
-import android.os.Handler
-import android.os.Looper
-import java.util.concurrent.Executor
-
 class Config<T : Any>(
     val cacheDir: String,
     val name: String = DEFAULT_NAME,
     val convertible: Fuse.DataConvertible<T>,
-    var diskCapacity: Long = 1024 * 1024 * 20,
-    var callbackExecutor: Executor = Executor {
-        if (Thread.currentThread() == Looper.getMainLooper().thread) {
-            it.run()
-        } else {
-            val mainLooperHandler = Handler(Looper.getMainLooper())
-            mainLooperHandler.post(it)
-        }
-    }
+    var diskCapacity: Long = 1024 * 1024 * 20
 ) {
 
     companion object {
