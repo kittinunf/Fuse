1ead194a5e7bb5c3631bf676c2fcbe9b3c1710e5
Kittinun Vantasin <kittinun.f@gmail.com>
2019-11-25 15:46:29 +0900

    Update android lru cache

--- fuse-android/src/main/java/com/github/kittinunf/fuse/android/MemLruCache.kt
+++ fuse-android/src/main/java/com/github/kittinunf/fuse/android/MemLruCache.kt
@@ -1,43 +1,35 @@
 package com.github.kittinunf.fuse.android
 
 import android.util.LruCache
-import com.github.kittinunf.fuse.core.cache.KeyType
+import com.github.kittinunf.fuse.core.cache.Entry
 import com.github.kittinunf.fuse.core.cache.Persistence
 
 class MemLruCache(private val cache: LruCache<String, Any>) : Persistence<Any> {
 
-    override fun put(safeKey: String, key: String, value: Any, timeToPersist: Long) {
-        cache.apply {
-            put(safeKey, value)
-            put(convertKey(safeKey, KeyType.Key.ordinal.toString()), key)
-            put(convertKey(safeKey, KeyType.Time.ordinal.toString()), timeToPersist)
-        }
+    override fun put(safeKey: String, entry: Entry<Any>) {
+        cache.put(safeKey, entry)
     }
 
-    override fun remove(safeKey: String): Boolean {
-        val removedValue = cache.remove(safeKey)
-        cache.remove(convertKey(safeKey, KeyType.Key.ordinal.toString()))
-        cache.remove(convertKey(safeKey, KeyType.Time.ordinal.toString()))
-        return removedValue != null
-    }
+    override fun remove(safeKey: String): Boolean = cache.remove(safeKey) != null
 
     override fun removeAll() {
         cache.evictAll()
     }
 
     override fun allKeys(): Set<String> {
-        return cache.snapshot().keys.filter { !it.contains(".") }
-            .map { get(convertKey(it, KeyType.Key.ordinal.toString())) as String }
-            .toSet()
+        return synchronized(this) {
+            val snapshot = cache.snapshot()
+            snapshot.keys.map { getEntry(it)!!.key }.toSet()
+        }
     }
 
     override fun size(): Long {
         return cache.size().toLong()
     }
 
-    override fun get(safeKey: String): Any? = cache.get(safeKey)
+    override fun get(safeKey: String): Any? = getEntry(safeKey)?.data
 
-    override fun getTimestamp(safeKey: String): Long? = get(convertKey(safeKey, KeyType.Time.ordinal.toString())) as? Long
+    override fun getTimestamp(safeKey: String): Long? = getEntry(safeKey)?.timestamp
 
-    private fun convertKey(key: String, suffix: String): String = "$key.$suffix"
+    private fun getEntry(safeKey: String): Entry<Any>? = cache.get(safeKey) as? Entry<Any>
 }
