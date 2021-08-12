c892a4cca0f7bdd6177e7f92f74a7bc1c00e55e2
Kittinun Vantasin <kittinun.f@gmail.com>
2021-08-13 01:27:36 +0900

    Remove unneccessary

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,12 +4,4 @@ plugins {
 
 repositories {
     mavenCentral()
-    google()
-    maven { setUrl("https://plugins.gradle.org/m2/") }
-}
-
-kotlin {
-    val main by sourceSets.getting {
-        kotlin.srcDir("buildSrc/src/main/kotlin")
-    }
 }
