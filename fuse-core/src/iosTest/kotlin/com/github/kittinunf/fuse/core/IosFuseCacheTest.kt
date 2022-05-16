21c6c1ea4329f6418aac6c4e04334f70fb61abcc
Kittinun Vantasin <kittinun.f@gmail.com>
2022-05-16 14:31:29 +0900

    Update with more API

--- fuse-core/src/iosTest/kotlin/com/github/kittinunf/fuse/core/IosFuseCacheTest.kt
+++ fuse-core/src/iosTest/kotlin/com/github/kittinunf/fuse/core/IosFuseCacheTest.kt
@@ -5,11 +5,13 @@ import kotlinx.serialization.BinaryFormat
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.builtins.ByteArraySerializer
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.modules.EmptySerializersModule
 import kotlinx.serialization.modules.SerializersModule
 import platform.Foundation.NSBundle
 import platform.Foundation.NSURL
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertIs
 import kotlin.test.assertNotNull
 
@@ -25,6 +27,20 @@ class IosFuseCacheTest {
 
             override fun <T> encodeToByteArray(serializer: SerializationStrategy<T>, value: T): ByteArray {
                 return value as ByteArray
+            }
+
+        }).build()
+
+    private val stringCache: Cache<String> =
+        IosConfig("test-string-cache", path = null, serializer = String.serializer(), formatter = object : BinaryFormat {
+            override val serializersModule: SerializersModule = EmptySerializersModule
+
+            override fun <T> decodeFromByteArray(deserializer: DeserializationStrategy<T>, bytes: ByteArray): T {
+                return bytes.decodeToString() as T
+            }
+
+            override fun <T> encodeToByteArray(serializer: SerializationStrategy<T>, value: T): ByteArray {
+                return (value as String).encodeToByteArray()
             }
 
         }).build()
@@ -45,6 +61,22 @@ class IosFuseCacheTest {
         val file = getUrlFromResource("sample_song", "mp3")
         val result = cache.put(file)
         assertIs<Result.Success<*>>(result)
+
+        val (value, _) = cache.get(file)
+        assertNotNull(value)
+    }
+
+    @Test
+    fun `should fetch data from file as string correctly`() {
+        val file = getUrlFromResource("lorem_ipsum", "txt")
+        val result = stringCache.put(file)
+        assertIs<Result.Success<*>>(result)
+
+        val (value, _) = stringCache.get(file)
+        assertEquals(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod orci at libero sollicitudin, quis mollis nunc rutrum. Proin orci arcu, faucibus non lorem eu, euismod lobortis ligula. Curabitur vehicula lorem nec aliquam mollis. Donec sit amet ligula quis nisi ullamcorper mattis nec eget justo. Sed nec nulla eu nunc hendrerit mattis. Morbi commodo dapibus nibh, eget mattis odio ornare non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi sed tellus id magna viverra viverra. Ut dignissim quis ligula ac efficitur. Maecenas eleifend faucibus laoreet. Etiam at justo in nulla eleifend cursus. Sed ut molestie ante, nec consectetur turpis. Curabitur imperdiet pharetra mauris at iaculis. Proin a mauris ex. Duis leo nisl, viverra vel mauris eu, convallis ultricies nisl. Nullam vehicula ullamcorper erat vitae dictum.",
+            value
+        )
     }
 
     private fun getUrlFromResource(name: String, type: String): NSURL {
--- fuse-core/src/jvmTest/kotlin/com/github/kittinunf/fuse/core/JvmFuseCacheTest.kt
+++ fuse-core/src/jvmTest/kotlin/com/github/kittinunf/fuse/core/JvmFuseCacheTest.kt
@@ -11,6 +11,7 @@ import kotlinx.serialization.modules.EmptySerializersModule
 import kotlinx.serialization.modules.SerializersModule
 import org.junit.Test
 import java.io.File
+import kotlin.test.assertEquals
 import kotlin.test.assertIs
 
 class JvmFuseCacheTest {
@@ -40,7 +41,6 @@ class JvmFuseCacheTest {
             override fun <T> encodeToByteArray(serializer: SerializationStrategy<T>, value: T): ByteArray {
                 return (value as String).encodeToByteArray()
             }
-
         }).build()
 
     @Test
@@ -58,6 +58,22 @@ class JvmFuseCacheTest {
         val file = getFileFromResource("./sample_song.mp3")
         val result = cache.put(file)
         assertIs<Result.Success<*>>(result)
+
+        val (value, _) = cache.get(file)
+        assertNotNull(value)
+    }
+
+    @Test
+    fun `should fetch data from file as string correctly`() {
+        val file = getFileFromResource("lorem_ipsum.txt")
+        val result = stringCache.put(file)
+        assertIs<Result.Success<*>>(result)
+
+        val (value, _) = stringCache.get(file)
+        assertEquals(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce euismod orci at libero sollicitudin, quis mollis nunc rutrum. Proin orci arcu, faucibus non lorem eu, euismod lobortis ligula. Curabitur vehicula lorem nec aliquam mollis. Donec sit amet ligula quis nisi ullamcorper mattis nec eget justo. Sed nec nulla eu nunc hendrerit mattis. Morbi commodo dapibus nibh, eget mattis odio ornare non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi sed tellus id magna viverra viverra. Ut dignissim quis ligula ac efficitur. Maecenas eleifend faucibus laoreet. Etiam at justo in nulla eleifend cursus. Sed ut molestie ante, nec consectetur turpis. Curabitur imperdiet pharetra mauris at iaculis. Proin a mauris ex. Duis leo nisl, viverra vel mauris eu, convallis ultricies nisl. Nullam vehicula ullamcorper erat vitae dictum.",
+            value
+        )
     }
 
     private fun getFileFromResource(name: String): File {
